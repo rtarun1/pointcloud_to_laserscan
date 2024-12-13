33ec5bf687aae94a01b51091424d8e391cbd2938
rtarun1 <tarun.ramak@gmail.com>
2024-12-13 22:55:45 +0530

    amcl working properly

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -14,13 +14,13 @@ def generate_launch_description():
             parameters=[{
                 'target_frame': 'laser_frame',
                 'transform_tolerance': 0.01,
-                'min_height': 0.7, #0
-                'max_height': 1.0, #1
+                'min_height': 0.1, #0
+                'max_height': 2.0, #1
                 'angle_min': -3.14, # -M_PI/ 2-1.5708
                 'angle_max': 3.14, # M_PI/2
                 'angle_increment': 0.0087, # M_PI/360.0
                 'scan_time': 0.3333,
-                'range_min': 0.45,
+                'range_min': 1.45,
                 'range_max': 50.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
